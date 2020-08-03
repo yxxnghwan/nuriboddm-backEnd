a7c2973529b33f79d12a83c9e72a1b8babd6461a
yxxnghwan <younghwan960@gmail.com>
2020-08-03 14:17:27 +0900

    add LoginManageMentService

--- src/main/java/com/nuribodeum/controller/AccountController.java
+++ src/main/java/com/nuribodeum/controller/AccountController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nuribodeum.mapper.AccountMapper;
+import com.nuribodeum.vo.AccountVO;
 import com.nuribodeum.vo.ManagerVO;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -31,22 +32,24 @@ public class AccountController {
 		accountMapper.insertManager(vo);
 	}
 	
-	@PostMapping("/auth/manager")
-	public ManagerVO loginManager(HttpServletRequest request, HttpServletResponse response, @RequestBody ManagerVO vo) {
+	@PostMapping("/auth")
+	public String loginManager(HttpServletRequest request, HttpServletResponse response, @RequestBody AccountVO vo) {
 		System.out.println("로그인하기");
-		// db에서 아이디로 찾아서 vo를 불러오고 그 vo 비밀번호랑 입력받은 password랑 checkpw해서 로그인성공 실패 하면댐
-		ManagerVO manager = accountMapper.getManager(vo.getManager_id());
-		if(manager == null) {
-			System.out.println("아이디 없음");
-		} else {
-			System.out.println("아이디 있음");
-			if(BCrypt.checkpw(vo.getPassword(), manager.getPassword())) { // 비밀번호 일치
-				System.out.println("로그인 성공");
-			} else { // 비밀번호 틀림
-				System.out.println("비밀번호 오류");
+		if(vo.getAccount_type().equals("manager")) {
+			// db에서 아이디로 찾아서 vo를 불러오고 그 vo 비밀번호랑 입력받은 password랑 checkpw해서 로그인성공 실패 하면댐
+			ManagerVO manager = accountMapper.getManager(vo.getId());
+			if(manager == null) {
+				System.out.println("아이디 없음");
+			} else {
+				System.out.println("아이디 있음");
+				if(BCrypt.checkpw(vo.getPassword(), manager.getPassword())) { // 비밀번호 일치
+					System.out.println("로그인 성공");
+				} else { // 비밀번호 틀림
+					System.out.println("비밀번호 오류");
+				}
 			}
+			return manager.getManager_id();
 		}
-		return manager;
-		
+		return "";
 	}
 }
